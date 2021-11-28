0950e16da26e404f6b1b73a89ff0f2211154c9cb
Franco Mozo <fm.login1@gmail.com>
2021-11-28 12:40:29 -0300

    :cyclone: reestructure of code into a state machine and pygame adittion for pause/play

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,10 @@
 import os
+#import keyboard
+import select
+import sys
 import time
+
+#import getch
 
 
 def timer(t, label):
@@ -9,6 +14,12 @@ def timer(t, label):
     """
     
     while t >= 0:
+        #sys.stdout.flush()
+        #ready = select.select([sys.stdin], [], [], 2)
+        #key = getch.getch()
+        #print("key:",key)
+        #time.sleep(2)
+    
         MM, SS = divmod(t, 60)
         HH, MM = divmod(MM, 60)
 
@@ -19,10 +30,13 @@ def timer(t, label):
             fstr = f' == {HH:02d}:{MM:02d}:{SS:02d} =='
             print(fstr, end="\r")
 
+        #if SS % 30 == 0:
+        #    with open("html/index.html", "w") as text_file:
+        #        text_file.write(fstr)
         
         time.sleep(1)
         print(" " * len(fstr), end="\r")
-        t -= 1
+        t -= 1 
     return
 
 def end_sound(freq, duration, times):
